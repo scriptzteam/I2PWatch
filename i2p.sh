e02e60b6e5e98466a2ca0618d59fbb6c70fb4c13
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2025-09-06 15:55:13 +0200

    Update i2p.sh

--- i2p.sh
+++ i2p.sh
@@ -3,5 +3,5 @@
 OUTPUT_FILE_ipv4="ipv4"
 OUTPUT_FILE_ipv6="ipv6"
 
-curl "$IPV4" > "$OUTPUT_FILE_ipv4"
-curl "$IPV6" > "$OUTPUT_FILE_ipv6"
+curl -s "$IPV4" > "$OUTPUT_FILE_ipv4"
+curl -s "$IPV6" > "$OUTPUT_FILE_ipv6"
